428990300773cf96e080f03c2d4eb00bd73df771
Hridhyapaul <hpdesign2021@gmail.com>
2023-05-15 20:11:12 +0600

    All is done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+// const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -28,6 +29,17 @@ async function run() {
         const serviceCollection = client.db("carDoctor").collection("services");
         const bookingCollection = client.db("carDoctor").collection("booking");
 
+        // JWT Routes...
+
+        // app.post('jwt', (req, res) => {
+        //     const user = req.body;
+        //     console.log(user)
+        //     const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1h' });
+        //     res.send(token);
+        // })
+
+        // Service Routes...
+
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -46,7 +58,7 @@ async function run() {
         })
 
 
-        // Booking...
+        // Booking Routes...
 
         app.get('/bookings', async (req, res) => {
             let query = {};
